18d38f4afdd6e4d859cea4eab8bd76a44c0f6e4e
unknown <Shadley@.(none)>
2011-03-11 02:30:11 +0200

    Changing Deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,141 +1,128 @@
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
 set :application, "yuoweb"
-set :repository,  "https://sanjuro@github.com/sanjuro/yuoweb.com.git"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
+set :domain, "yuoweb.com"
 set :deploy_to, "/home/yuowebco/"
 
-# If there's no access to the repository from the production server, deploy via uploading tarball to the server
-#set :deploy_via, :copy
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
+# =============================================================================
+# SCM OPTIONS
+# =============================================================================
 set :scm, :git
+#set :scm_user, "username" # optional
+#set :scm_password, "password" # optional
+set :repository,  "https://sanjuro@github.com/sanjuro/yuoweb.com.git"
 
+# =============================================================================
+# SSH OPTIONS
+# =============================================================================
+set :user, "username"
+set :use_sudo, false # optional
+
+# =============================================================================
+# ROLES
+# =============================================================================
+# Modify these values to execute tasks on a different server.
 role :app, "yuoweb.com"
 role :web, "yuoweb.com"
 role :db,  "yuoweb.com", :primary => true
 
-set :user, "codingspree"
+# =============================================================================
+# CAPISTRANO OPTIONS
+# =============================================================================
+set :keep_releases, 3
+set :deploy_via, :remote_cache
 
-# path to php executable 
-set :php, "/usr/local/php5/bin/php5"
 
-# symfony application name (used for migrations)
-set :sf_app, "frontend"
+set :app_symlinks, %w{uploads} # dirs that need to remain the same between deploys
 
-namespace (:deploy) do
-
-  desc <<-DESC
-    [internal] Overriding original task to fit to symfony project needs
-  DESC
+# =============================================================================
+# OVERWRITE CAPISTRANO TASKS
+# =============================================================================
+namespace :deploy do
+  desc "Overwrite the start task to set the permissions on the project."
+  task :start do
+    run "php #{release_path}/symfony project:permissions"
+    run "php #{release_path}/symfony doctrine:build --all --no-confirmation"
+  end
+  
+  desc "Overwrite the restart task because symfony doesn't need it."
+  task :restart do ; end
+  
+  desc "Overwrite the stop task because symfony doesn't need it."
+  task :stop do ; end
+  
+  desc "Customize migrate task to work with symfony."
+  task :migrate do
+    run "php #{latest_release}/symfony doctrine:migrate --env=prod"
+  end
+  
+  desc "Symlink static directories that need to remain between deployments."
+  task :create_dirs do
+    if app_symlinks
+      app_symlinks.each do |link|
+        run "mkdir -p #{shared_path}/system/#{link}"
+        run "ln -nfs #{shared_path}/system/#{link} #{release_path}/web/#{link}"
+      end
+    end
+  end
+  
+  desc "Customize the finalize_update task to work with symfony."
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
 
     run <<-CMD
-      rm -rf #{latest_release}/log &&
-      ln -s #{shared_path}/log #{latest_release}/log
-    CMD
-    
-    run <<-CMD
-      rm -rf #{latest_release}/cache &&
+      rm -rf #{latest_release}/log #{latest_release}/cache &&
+      mkdir -p #{shared_path}/cache &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
       ln -s #{shared_path}/cache #{latest_release}/cache
     CMD
-    
-    stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-    asset_paths = %w(images css js).map { |p| "#{latest_release}/web/#{p}" }.join(" ")
-    run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = %w(css images js).map { |p| "#{latest_release}/web/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    end
   end
-  
-  desc <<-DESC
-    Overriding original task to exclude restart
-  DESC
-  task :default do
+
+  desc "Need to overwrite the deploy:cold task so it doesn't try to run the migrations."
+  task :cold do
     update
+    start
   end
   
-  desc <<-DESC
-    Overriding original task to use symfoy migrations (via sfMigrationsLightPlugin)
-  DESC
-  task :migrations do
-    update
-    sf.migrate
-  end  
-  
-  after "deploy:update", 'deploy:customize'
-  
-  desc <<-DESC
-    All custom tasks will be here
-  DESC
-  task :customize do
-    # custmize it here
-    sf.symlinks
-    sf.remove_dev_environments
-    
-    # clear cache
-    sf.cc
+  desc "Task to run all the tests for the application."
+  task :symfony_test do
+    run "php #{latest_release}/symfony test:all"
+  end
+
+  desc "Task used in conjunction with the symfony_test task to rebuild the database."
+  task :rebuild do
+    run "php #{latest_release}/symfony doctrine:build --all --and-load --env=test --no-confirmation"
   end
   
+  desc "Deploy the application and run the test suite."
+  task :testall do
+    update_code
+    symlink
+    rebuild
+    symfony_test
+  end
 end
 
-
-namespace (:sf) do
-  
-  desc <<-DESC
-    Run the "symfony migrate" task
-  DESC
-  task :migrate do
-    run "cd #{current_path} && #{php} symfony migrate #{sf_app}"    
+namespace :symlink do
+  desc "Symlink the database"
+  task :db do
+    run "ln -nfs #{shared_path}/databases.yml #{release_path}/config/databases.yml"
   end
-
-  desc <<-DESC
-    Run the "symfony cc" task
-  DESC
-  task :cc do
-    run "cd #{current_path} && rm -rf cache/*"
-  end
-
-  desc <<-DESC
-    Create symlink to symfony specific targets
-  DESC
-  task :symlinks do
-    # symlink to database.yml
-    run "rm -rf #{current_path}/config/databases.yml"
-    run "ln -s #{shared_path}/databases.yml #{current_path}/config/databases.yml"
-
-    # symlink to config.php    
-    run "rm -rf #{current_path}/config/config.php"
-    run "ln -s #{shared_path}/config.php #{current_path}/config/config.php"
-
-    # symlink to sf data dir    
-    run "rm -rf #{current_path}/web/sf"
-    run "ln -s /path/to/sf/data/dir #{current_path}/web/sf"
-        
-    # symlink to uploads    
-    run "rm -rf #{current_path}/web/uploads"
-    run "ln -s #{shared_path}/uploads #{current_path}/web/uploads"
-    
-  end 
-  
-  desc <<-DESC
-    Remove DEV environments
-  DESC
-  task :remove_dev_environments do
-    run "rm -rf #{current_path}/web/*_dev.php"    
-  end 
-  
-  desc <<-DESC
-    Disable symfony application
-  DESC
-  task :disable do
-    run "cd #{current_path} && #{php} symfony disable #{sf_app} prod"    
-  end 
-  
-  desc <<-DESC
-    Enable symfony application
-  DESC
-  task :enable do
-    run "cd #{current_path} && #{php} symfony enable #{sf_app} prod"    
-  end   
 end
+
+namespace :symfony do
+  desc "Task to clear the cache on deploy."
+  task :clear_cache do
+    run "php #{release_path}/symfony cache:clear"
+  end
+end
+
+after "deploy:finalize_update", "symlink:db", "deploy:create_dirs", "symfony:clear_cache"
+
