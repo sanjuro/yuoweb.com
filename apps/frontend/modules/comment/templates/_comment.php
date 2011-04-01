3113d8fe583e08574c0c559adeed9e76ceb2dcb4
Shadley <shad6ster@gmail.com>
2011-04-01 09:00:21 +0200

    Adding Missing form file

--- apps/frontend/modules/comment/templates/_comment.php
+++ apps/frontend/modules/comment/templates/_comment.php
@@ -2,7 +2,6 @@
     <tbody class="comment<?php if($obj->is_delete) echo " deleted"; ?>">
       <tr<?php if($first_line) echo ' class="first_line"' ?>>
         <?php include_partial("comment/comment_author", array('website' => $obj->getWebsite(), 'name' => $obj->getAuthor(), 'date' => $obj->getCreatedAt())) ?>
-
       </tr>
       <?php include_partial("comment/comment_body", array('obj' => $obj)) ?>
     </tbody>
---
+++ lib/form/FrontendCommentForm.class.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * PluginComment form.
+ *
+ * @package    vjCommentPlugin
+ * @subpackage form
+ * @author     Jean-Philippe MORVAN <jp.morvan@ville-villejuif.fr>
+ * @version    SVN: $Id$
+ */
+
+class FrontendCommentForm extends PluginCommentCommonForm
+{
+  public $currentUser;
+	
+  public function setup()
+  {
+    parent::setup();
+    
+    $user = $this->getOption('user');
+    
+    $this->currentUser = $user;
+    
+    $fields = array(
+      'body'
+    );
+    if(vjComment::isGuardBindEnabled())
+    {
+      $fields[] = 'user_id';
+    }
+    $this->useFields($fields);
+    
+    $this->widgetSchema['reply_author'] = new sfWidgetFormInputText(array(), array('readonly' => "readonly"));
+    $this->widgetSchema->setLabel('reply_author', __('Reply to', array(), 'vjComment'));
+    $this->widgetSchema->setHelp('author_email', __('Your email will never be published', array(), 'vjComment'));
+    $this->widgetSchema['user_id'] = new sfWidgetFormInputHidden();
+
+    if( vjComment::isUserBoundAndAuthenticated($user) )
+    {
+        unset( $this['author_email'], $this['author_website'], $this['author_name'] );
+    }
+    else
+    {
+        unset( $this['user_id'] );
+    }
+    if (vjComment::isCaptchaEnabled() && !vjComment::isUserBoundAndAuthenticated($user) )
+    {
+      $this->addCaptcha();
+    }
+    $this->widgetSchema->setNameFormat($this->getOption('name').'[%s]');
+  }
+  
+  public function updateObject($values = null)
+  { 
+    if (is_null($values))
+    {
+      $values = $this->values;
+    }
+    
+	$NetworkUser = Doctrine_Core::getTable('NetworkUser')->findOneById($this->currentUser->getNetworkUserId());
+	
+	$sfGuardUser = $NetworkUser->getsfGuardUserObject();
+	
+	//$values['author_email'] = $sfGuardUser->getEmailAddress();
+    
+    //$values['author_name'] = $sfGuardUser->getUsername();
+	
+	$values = $this->processValues($values);
+		 
+	$this->object->fromArray($values);
+	   	 
+	$this->updateObjectEmbeddedForms($values);
+	   	 
+	return $this->object;	
+  }
+
+  protected function addCaptcha()
+  {
+    $this->widgetSchema['captcha'] = new sfWidgetFormReCaptcha(array(
+      'public_key' => sfConfig::get('app_recaptcha_public_key')
+    ));
+
+    $this->validatorSchema['captcha'] = new sfValidatorReCaptcha(array(
+      'private_key' => sfConfig::get('app_recaptcha_private_key')
+    ));
+    $this->validatorSchema['captcha']
+        ->setMessage('captcha', __('The captcha is not valid (%error%).', array(), 'vjComment'))
+        ->setMessage('server_problem', __('Unable to check the captcha from the server (%error%).', array(), 'vjComment'));
+  }
+}
