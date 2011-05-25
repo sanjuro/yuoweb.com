6f7fb0caaddd459798e6a660e0bc10b25617a95f
Shadley Wentzel <shad6ster@gmail.com>
2011-05-25 11:17:48 +0200

    Adding new forms for following

--- apps/backend/modules/user/templates/_list.php
+++ apps/backend/modules/user/templates/_list.php
@@ -1,42 +1,1 @@
-<table class="users">
-  <tr>
-  	<th>Pic</th>
-  	<th>Username</th>
-  	<th>Fullname</th>
-  	<th>Gender</th>
-  	<th>City</th>
-  	<th>Country</th>
-  	<th></th>
-  	<th></th>
-  <tr>
-  <?php foreach ($users as $i => $user): ?>
-    <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>" valign="middle" onmouseover="this.style.backgroundColor='#F2F2F2';" onmouseout="this.style.backgroundColor='white';" style="background-color: rgb(242, 242, 242); ">
-      <td class="pic">
-        <img src="<?php echo "http://".$_SERVER['SERVER_NAME'].$sf_request->getRelativeUrlRoot()."/uploads/pictures/50x50/".$user['sfGuardUser']['UserProfile'][0]['profile_pic'] ?>">
-      </td>
-      <td class="username">
-        <?php echo $user['sfGuardUser']['username'] ?>
-      </td>
-      <td class="fullname">
-        <?php echo ucwords($user['sfGuardUser']['first_name']).' '.ucwords($user['sfGuardUser']['last_name']) ?>
-      </td>
-      <td class="email">
-        <?php if($user['sfGuardUser']['UserProfile'][0]['gender'] == 1)  :?>
-        	Male
-        <?php elseif($user['sfGuardUser']['UserProfile'][0]['gender'] == 2) : ?>
-        	Female
-        <?php else : ?>
-        	n/a
-        <?php endif;?>
-      </td>
-      <td class="city">
-        <?php echo $user['sfGuardUser']['UserProfile'][0]['city'] ?>
-      </td>
-      <td class="country">
-        <?php echo $user['sfGuardUser']['UserProfile'][0]['country'] ?>
-      </td>
-      <td></td>
-      <td></td>
-    </tr>
-  <?php endforeach; ?>
-</table>
+<table class="users">  <tr>  	<th>Pic</th>  	<th>Username</th>  	<th>Fullname</th>  	<th>Gender</th>  	<th>City</th>  	<th>Country</th>  	<th></th>  	<th></th>  <tr>  <?php foreach ($users as $i => $user): ?>    <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>" valign="middle" onmouseover="this.style.backgroundColor='#F2F2F2';" onmouseout="this.style.backgroundColor='white';" style="background-color: rgb(242, 242, 242); ">      <td class="pic">        <img src="<?php echo "http://".$_SERVER['SERVER_NAME'].$sf_request->getRelativeUrlRoot()."/uploads/pictures/50x50/".$user['sfGuardUser']['UserProfile'][0]['profile_pic'] ?>">      </td>      <td class="username">        <?php echo $user['sfGuardUser']['username'] ?>      </td>      <td class="fullname">        <?php echo ucwords($user['sfGuardUser']['first_name']).' '.ucwords($user['sfGuardUser']['last_name']) ?>      </td>      <td class="email">        <?php if($user['sfGuardUser']['UserProfile'][0]['gender'] == 1)  :?>        	Male        <?php elseif($user['sfGuardUser']['UserProfile'][0]['gender'] == 2) : ?>        	Female        <?php else : ?>        	n/a        <?php endif;?>      </td>      <td class="city">        <?php echo $user['sfGuardUser']['UserProfile'][0]['city'] ?>      </td>      <td class="country">        <?php echo $user['sfGuardUser']['UserProfile'][0]['country'] ?>      </td>      <td></td>      <td></td>    </tr>  <?php endforeach; ?></table>
---
+++ lib/form/doctrine/FollowIndexForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * FollowIndex form.
+ *
+ * @package    Yuoweb
+ * @subpackage form
+ * @author     Shadley Wentzel
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class FollowIndexForm extends BaseFollowIndexForm
+{
+  public function configure()
+  {
+  }
+}
